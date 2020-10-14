3bae063a5d8c84243256d7a3900cb37848c8aec1
pierbr03 <72429715+pierbr03@users.noreply.github.com>
2020-10-13 20:48:20 -0700

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,17 @@
+function myFunction() {
+	alert("Do Not Click This Button!!");
+	}
+
+
+
+
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	docuent.getElementById("Irish") .innerHTML = 
+	result;
+	
+	}
+	
+	
